6a3a28f124d75333c8715781c094c3b9937a079c
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-02-12 15:16:08 +0000

    t

--- src/components/block/ScriptBlock.ts
+++ src/components/block/ScriptBlock.ts
@@ -2,19 +2,24 @@ import { world } from "@minecraft/server";
 import { BlockCustomComponentV2 } from "./BlockCustomComponentV2";
 
 const componentsToRegister: [string, BlockCustomComponentV2][] = [];
-
-export class ScriptBlock implements BlockCustomComponentV2 {
-    constructor(blockID: string) {
-        componentsToRegister.push([blockID, this]);
-    }
-}
-
-world.beforeEvents.worldInitialize.subscribe(e => {
-    componentsToRegister.forEach(component => {
-        e.blockComponentRegistry.registerCustomComponent(component[0], component[1]);
-    })
-})
+let hasInitialized = false;
 
 export function registerBlockComponent(componentID: string, component: BlockCustomComponentV2) {
+    if (!hasInitialized) {
+        world.beforeEvents.worldInitialize.subscribe(e => {
+            componentsToRegister.forEach(component => {
+                try {
+                    e.blockComponentRegistry.registerCustomComponent(component[0], component[1]);
+                }
+                catch (e) {
+                    console.error(`Failed to automatically register block component ${component[0]}`);
+                    console.error(e);
+                };
+            })
+        });
+
+        hasInitialized = true;
+    }
+
     componentsToRegister.push([componentID, component]);
 }
---
+++ src/mixins/world/Dimension.ts
@@ -0,0 +1,31 @@
+import { Player, Vector3, Dimension } from "@minecraft/server";
+import { Vec3 } from "../../Index";
+
+declare module "@minecraft/server" {
+    interface Dimension {
+        /**
+         * Finds the closest player
+         * @param position The position to search from
+         * @param maxDistance The max distance to search for players
+         * @returns If the closest player is further than maxDistance, or no players are in this dimension, undefined is returned
+         */
+        getClosestPlayer(position: Vector3, maxDistance?: number): Player | undefined;
+    }
+}
+
+Dimension.prototype.getClosestPlayer = function(position: Vector3, maxDistance: number = 100): Player | undefined {
+    let closestPlayer: Player | undefined = undefined;
+    let closestDistance = Number.POSITIVE_INFINITY;
+
+    for (const player of this.getPlayers()) {
+        const distance = Vec3.distance(position, player.location);
+
+        if (distance < closestDistance) {
+            closestPlayer = player;
+            closestDistance = distance;
+        }
+    }
+
+    if (closestDistance > maxDistance) return undefined;
+    return closestPlayer;
+}
