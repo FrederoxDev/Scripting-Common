963e9503f362563da4bcbcbee96571f27e054c99
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-04-10 20:34:48 +0100

    isInCreative & getHeldItem

--- src/mixins/entity/PlayerHelpers.ts
+++ src/mixins/entity/PlayerHelpers.ts
@@ -1,12 +1,23 @@
-import { Entity, EntityComponent, EntityComponentTypes, EntityEquippableComponent, EntityInventoryComponent, EquipmentSlot, Player } from "@minecraft/server";
+import { Entity, EntityComponent, EntityComponentTypes, EntityEquippableComponent, EntityInventoryComponent, EquipmentSlot, GameMode, Player } from "@minecraft/server";
 
 declare module "@minecraft/server" {
     interface Player {
         getMainhand(): ContainerSlot;
+        getHeldItem(): ItemStack | undefined;
+        isInCreative(): boolean;
+
     }
 }
 
 
 Player.prototype.getMainhand = function() {
     return this.getEquippable().getEquipmentSlot(EquipmentSlot.Mainhand);
+}
+
+Player.prototype.isInCreative = function() {
+    return this.getGameMode() === GameMode.creative;
+}
+
+Player.prototype.getHeldItem = function() {
+    return this.getEquippable().getEquipment(EquipmentSlot.Mainhand);
 }
