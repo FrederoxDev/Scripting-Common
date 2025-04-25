cb8209b7f5e329419c7251293758c366503ebf57
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-04-25 09:44:03 +0100

    add getEntitiesPrecise

--- src/mixins/block/BlockEntity.ts
+++ src/mixins/block/BlockEntity.ts
@@ -1,14 +1,35 @@
 import { Block } from "@minecraft/server";
+import { Vec3 } from "../../Index";
 
 declare module "@minecraft/server" {
     interface Block {
+        /**
+         * Returns all entities at the block location with the given identifier.
+         * - not as precise as getEntitiesPrecise. two neighbouring blocks can return the same entity.
+         * - in scenarios where this is not wanted use {@link getEntitiesPrecise} instead.
+         * @param identifier 
+         */
         getEntities(identifier: string): Entity[];
+
+
         getBlockEntity(identifier: string): Entity | undefined;
+
+        /**
+         * Returns all entities at the block location with the given identifier.
+         * - more precise than getEntities. two neighbouring blocks will not return the same entity.
+         * - in scenarios where this overhead is not needed use {@link getEntities}
+         */
+        getEntitiesPrecise(identifier: string): Entity[];
     }
 }
 
 Block.prototype.getEntities = function(identifier) {
     return this.dimension.getEntitiesAtBlockLocation(this.location).filter(e => e.typeId === identifier);
+} 
+
+Block.prototype.getEntitiesPrecise = function(identifier) {
+    return this.dimension.getEntitiesAtBlockLocation(this.location)
+        .filter(e => e.typeId === identifier && Vec3.equals(this.location, Vec3.floor(e.location)));
 } 
 
 Block.prototype.getBlockEntity = function(identifier) {
--- src/utils/error/BetterLogs.ts
+++ src/utils/error/BetterLogs.ts
@@ -1,14 +1,19 @@
 import { Block, Entity, ItemStack } from "@minecraft/server";
-import { Vec3 } from "../math/Vec3";
-
 const oldConsoleLog = console.log
 
-function deepInspect(obj: any, depth = 3, seen = new WeakSet()) {
+function deepInspect(obj: any, depth = 3, seen = new WeakSet()): string {
     if (depth <= 0) return "...";
     if (obj === null) return "null";
     if (obj === undefined) return "undefined";
+
+    if (Array.isArray(obj)) {
+        const strings = obj.map((item) => deepInspect(item, depth - 1, seen));
+        return `[${strings.join(", ")}]`;
+    }
+
     if (typeof obj !== "object") return String(obj);
 
+    // seen can only hold objects
     if (seen.has(obj)) return "[Circular]";
     seen.add(obj);
 
@@ -50,27 +55,6 @@ function deepInspect(obj: any, depth = 3, seen = new WeakSet()) {
 }
 
 console.log = function (...args) {
-    // const newArgs = args.map((arg) => {
-    //     if (arg instanceof Entity) {
-    //         return `[Entity: ${arg.typeId}, id: ${arg.id}]`;
-    //     }
-
-    //     if (arg instanceof Block) {
-    //         return `[Block: ${arg.typeId}, at: (${arg.location.x}, ${arg.location.y}, ${arg.location.z})]`;
-    //     }
-        
-    //     if (arg instanceof ItemStack) {
-    //         return `[ItemStack: ${arg.typeId}, count: ${arg.amount}]`;
-    //     }
-
-    //     if (typeof arg === 'object' && arg !== null) {
-    //         return JSON.stringify(arg);
-    //     }
-
-    //     return arg;
-    // });
-
     const newArgs = args.map((arg) => deepInspect(arg, 3, new WeakSet()));
-
     oldConsoleLog.apply(console, newArgs);
 };
