b7c42f1bf82c468da53e55fdf18c9730f666bf15
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-07-24 17:18:07 +0100

    ItemStack::takeAmount, ItemStack::tryAddAmount

--- src/mixins/inventory/ItemStack.ts
+++ src/mixins/inventory/ItemStack.ts
@@ -1,4 +1,5 @@
 import { ItemStack } from "@minecraft/server";
+import { assert } from "../../utils/error/Error";
 
 declare module "@minecraft/server" {
     interface ItemStack {
@@ -9,6 +10,22 @@ declare module "@minecraft/server" {
          * @return A new ItemStack with the merged amount, or undefined if merging is not possible.
          */
         tryMerge(other: ItemStack): ItemStack | undefined;
+
+        /**
+         * Takes a specified amount from the item stack.
+         * @param amount The amount to take from the stack.
+         * @returns A stack with the amount taken
+         * @throws Error if the amount is greater than the current stack amount.
+         */
+        takeAmount(amount: number): ItemStack | undefined;
+
+
+        /**
+         * Adds a specified amount to the item stack.
+         * @param amount The amount to add to the stack.
+         * @returns The item stack with the new amount or undefined if the amount exceeds the maximum stack size.
+         */
+        tryAddAmount(amount: number): ItemStack | undefined;
     }
 }
 
@@ -16,6 +33,25 @@ ItemStack.prototype.tryMerge = function(other) {
     if (!this.isStackableWith(other)) return undefined;
 
     const totalAmount = this.amount + other.amount;
+    if (totalAmount > this.maxAmount) return undefined;
+
+    const newItem = this.clone();
+    newItem.amount = totalAmount;
+    return newItem;
+}
+
+ItemStack.prototype.takeAmount = function(amount) {
+    assert(this.amount >= amount, "Cannot take more than the current stack amount");
+
+    if (this.amount === amount) return undefined;
+
+    const newItem = this.clone();
+    newItem.amount -= amount;
+    return newItem;
+}
+
+ItemStack.prototype.tryAddAmount = function(amount) {
+    const totalAmount = this.amount + amount;
     if (totalAmount > this.maxAmount) return undefined;
 
     const newItem = this.clone();
